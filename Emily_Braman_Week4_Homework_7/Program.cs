79b9f26115cd9c8cc05f3df35f0ab34a389f3456
Emily Braman <embraman10@gmail.com>
2021-02-03 20:36:14 -0500

    enter digits to be inputted

--- Emily_Braman_Week4_Homework_7/Program.cs
+++ Emily_Braman_Week4_Homework_7/Program.cs
@@ -6,7 +6,12 @@ namespace Emily_Braman_Week4_Homework_7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number;
+            int digit1;
+            int digit2;
+            int digit3;
+            int digit4;
+            int digit5;
         }
     }
 }
