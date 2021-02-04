475de7d74456484c15b7faaaf295fc4490eb51da
Emily Braman <embraman10@gmail.com>
2021-02-03 20:39:55 -0500

    determining five digits

--- Emily_Braman_Week4_Homework_7/Program.cs
+++ Emily_Braman_Week4_Homework_7/Program.cs
@@ -12,6 +12,14 @@ namespace Emily_Braman_Week4_Homework_7
             int digit3;
             int digit4;
             int digit5;
+            Console.WriteLine("Enter five-digit interger: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            digit1 = number / 10000;
+            digit2 = number % 10000 / 1000;
+            digit3 = number % 1000 / 100;
+            digit4 = number % 100 / 10;
+            digit5 = number % 10;
+
         }
     }
 }
